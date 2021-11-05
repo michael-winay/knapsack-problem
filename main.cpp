201ca6f22d9b178a0c702d7ab72f09a38b9c315f
Mike <mgwinay@gmail.com>
2021-11-05 14:26:44 -0400

    Updated to have a more verbose output

--- main.cpp
+++ main.cpp
@@ -25,13 +25,15 @@ int* knapsack(int arr[4][2], int cap) {
 	int totalValue = 0;
 	int totalWeight = 0;
 
+	bool valid = false;
 	//size will have a max of 4
 	int size = 0;
 
 	//array to store the best possible subset
 	int best[4] = { 5,5,5,5 };
 
-	for (int i = 0; i < 16; i++) {
+	for (int i = 1; i < 16; i++) {
+		cout << "Subset" << " " << i << endl;
 
 		//array to store current subset being observed
 		int temp[4] = { 0,0,0,0 };
@@ -39,19 +41,22 @@ int* knapsack(int arr[4][2], int cap) {
 		for (int j = 0; j < 4; j++) {
 
 			if ((i & (1 << j)) != 0) {
-
+				cout << "[" << arr[j][0] << ", " << arr[j][1] << "]\n";
 				//check to see if adding subset value to array will put it over the capacity
 				int test = totalWeight + arr[j][0];
 				if (test > cap) {
+					valid = false;
 					continue;
 				}
 				if (test <= cap) {
+					valid = true;
 					totalWeight += arr[j][0];
 					temp[size] = j;
 					size++;
 				}
 			}
 		}
+		cout << "Subset" << " " << i << " " << "is" << " " << (valid ? "valid" : "invalid") << "\n\n";
 		//summing value of current subset
 		for (int i = 0; i < size; i++) {
 			totalValue += arr[temp[i]][1];
@@ -91,12 +96,12 @@ int main() {
 
 	cout << "Knapack capacity is" << " " << cap << endl;
 	cout << "Possible contents to put in knapsack:\n";
-	cout << "WEIGHT/VALUE\n";
+	cout << "[WEIGHT,VALUE]\n";
 	cout << "[" << myArray[0][0] << ", " << myArray[0][1] << "]" << endl;
 	cout << "[" << myArray[1][0] << ", " << myArray[1][1] << "]" << endl;
 	cout << "[" << myArray[2][0] << ", " << myArray[2][1] << "]" << endl;
 	cout << "[" << myArray[3][0] << ", " << myArray[3][1] << "]" << endl;
-	cout << "\n\n";
+	cout << endl;
 
 	//set capacity to eight for best results, theoretically could be any number
 	knapsack(myArray, cap);
